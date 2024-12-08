a0cb1a3c29f7425e2c468345f3db64904423614a
soheba <sohebaakter9@gmail.com>
2024-12-08 21:13:01 +0600

    problem added

--- CSES Problems/Apartments.cpp
+++ CSES Problems/Apartments.cpp
@@ -5,11 +5,15 @@
 
 using namespace std;
 
-// There are n applicants and m free apartments. Your task is to distribute the apartments so that as many applicants as possible will get an apartment.
-// Each applicant has a desired apartment size, and they will accept any apartment whose size is close enough to the desired size.
+// There are n applicants and m free apartments. Your task is to distribute the apartments so that as many applicants
+// as possible will get an apartment.
+// Each applicant has a desired apartment size, and they will accept any apartment whose size is close enough to the
+// desired size.
 // Input
-// The first input line has three integers n, m, and k: the number of applicants, the number of apartments, and the maximum allowed difference.
-// The next line contains n integers a_1, a_2, \ldots, a_n: the desired apartment size of each applicant. If the desired size of an applicant is x, he or she will accept any apartment whose size is between x-k and x+k.
+// The first input line has three integers n, m, and k: the number of applicants, the number of apartments, and the
+// maximum allowed difference.
+// The next line contains n integers a_1, a_2, \ldots, a_n: the desired apartment size of each applicant.
+// If the desired size of an applicant is x, he or she will accept any apartment whose size is between x-k and x+k.
 // The last line contains m integers b_1, b_2, \ldots, b_m: the size of each apartment.
 // Output
 // Print one integer: the number of applicants who will get an apartment.
