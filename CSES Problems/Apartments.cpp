d39920ef23983eed3b674f61a16ece626bca0c1f
Muasarrat Chowdhury <67744581+musarratChowdhury@users.noreply.github.com>
2024-12-08 19:08:14 +0600

    problems added

---
+++ CSES Problems/Apartments.cpp
@@ -0,0 +1,44 @@
+//
+// Created by User on 11/10/2024.
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// There are n applicants and m free apartments. Your task is to distribute the apartments so that as many applicants as possible will get an apartment.
+// Each applicant has a desired apartment size, and they will accept any apartment whose size is close enough to the desired size.
+// Input
+// The first input line has three integers n, m, and k: the number of applicants, the number of apartments, and the maximum allowed difference.
+// The next line contains n integers a_1, a_2, \ldots, a_n: the desired apartment size of each applicant. If the desired size of an applicant is x, he or she will accept any apartment whose size is between x-k and x+k.
+// The last line contains m integers b_1, b_2, \ldots, b_m: the size of each apartment.
+// Output
+// Print one integer: the number of applicants who will get an apartment.
+// Constraints
+//
+// 1 \le n, m \le 2 \cdot 10^5
+// 0 \le k \le 10^9
+// 1 \le a_i, b_i \le 10^9
+//
+// Example
+// Input:
+// 4 3 5
+// 60 45 80 60
+// 30 60 75
+//
+// Output:
+// 2
+const int N = 2e5+10;
+int a[N];
+
+
+int main() {
+
+    int n, m, k;
+    cin >> n >> m >> k;
+
+
+    for(int i = 0; i < n; i++) {
+
+    }
+
+}
