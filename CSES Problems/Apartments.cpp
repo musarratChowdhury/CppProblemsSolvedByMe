559827f6bf42785d12f930fa7ab5f57c3140f3d8
Muasarrat Chowdhury <67744581+musarratChowdhury@users.noreply.github.com>
2024-12-09 17:54:02 +0600

    Apartments.cpp problem solved

--- CSES Problems/Apartments.cpp
+++ CSES Problems/Apartments.cpp
@@ -5,44 +5,50 @@
 
 using namespace std;
 
-// There are n applicants and m free apartments. Your task is to distribute the apartments so that as many applicants
-// as possible will get an apartment.
-// Each applicant has a desired apartment size, and they will accept any apartment whose size is close enough to the
-// desired size.
-// Input
-// The first input line has three integers n, m, and k: the number of applicants, the number of apartments, and the
-// maximum allowed difference.
-// The next line contains n integers a_1, a_2, \ldots, a_n: the desired apartment size of each applicant.
-// If the desired size of an applicant is x, he or she will accept any apartment whose size is between x-k and x+k.
-// The last line contains m integers b_1, b_2, \ldots, b_m: the size of each apartment.
-// Output
-// Print one integer: the number of applicants who will get an apartment.
-// Constraints
-//
-// 1 \le n, m \le 2 \cdot 10^5
-// 0 \le k \le 10^9
-// 1 \le a_i, b_i \le 10^9
-//
-// Example
-// Input:
-// 4 3 5
-// 60 45 80 60
-// 30 60 75
-//
-// Output:
-// 2
+// https://cses.fi/problemset/task/1084
+
 const int N = 2e5+10;
 int a[N];
 
 
 int main() {
 
+    // number of applicants = n;
+    // number of free apartments = m;
+    // maximum allowed difference = k;
+
     int n, m, k;
     cin >> n >> m >> k;
 
+    vector<int> applicants(n);
+    vector<int> freeApartments(m);
 
     for(int i = 0; i < n; i++) {
-
+        cin >> applicants[i];
     }
 
+    for(int i = 0; i < m; i++) {
+        cin >> freeApartments[i];
+    }
+
+    sort(applicants.begin(), applicants.end()); // n (log n)
+    sort(freeApartments.begin(), freeApartments.end()); // m (log m)
+
+    int i = 0, j = 0;
+    int matches = 0;
+
+    while(i < n && j < m) {
+        if(applicants[i] - k > freeApartments[j]) {
+            j++;
+        }
+        else if(applicants[i] + k < freeApartments[j]) {
+            i++;
+        }else {
+            i++;
+            j++;
+            matches++;
+        }
+    }
+
+    cout << matches << endl;
 }
