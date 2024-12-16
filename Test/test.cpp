747f4f2fd7556b9efb29c476115c29c19f4f9274
Muasarrat Chowdhury <67744581+musarratChowdhury@users.noreply.github.com>
2024-12-16 10:30:13 +0600

    plus one problem added

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,14 +1,35 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+int removeElement(vector<int>& nums, int val) {
+    vector<int> unexpectedNums;
+    vector<int> result;
+    int s = 0;
 
+    for(int i = 0; i<nums.size(); i++){
+        if(nums[i]==val) unexpectedNums.push_back(nums[i]);
+        else result.push_back(nums[i]);
+    };
+
+    s = result.size();
+
+    for(int i = 0; i<unexpectedNums.size(); i++){
+        result.push_back(unexpectedNums[i]);
+    }
+    nums = result;
+    return s;
+}
 
 int main(){
     
     int x = 100;
-     cin >> x;
+    //  cin >> x;
+    //
+    // cout<<x << endl;
 
-    cout<<x << endl;
-
+    vector<int> v  = {3,2,2,3};
+    removeElement(v, 3);
     return 0;
+
 }
+
