fb8e395049213c7fbc77906f01b43e52518972b2
musarratChowdhury <muhitlopez143@gmail.com>
2025-06-07 20:45:20 +0600

    leetcode subbmission added

--- LeetCode/trapping_rain_water/1.cpp
+++ LeetCode/trapping_rain_water/1.cpp
@@ -2,7 +2,33 @@
 
 using namespace std;
 
+//for leet code submission
+int trap(vector<int>& height) {
+    int N = height.size();
+    if (N == 0) return 0;
 
+    vector<int> valley_L(N);
+    vector<int> valley_R(N);
+
+    valley_L[0] = height[0];
+    for (int i = 1; i < N; ++i) {
+        valley_L[i] = max(valley_L[i - 1], height[i]);
+    }
+
+    valley_R[N - 1] = height[N - 1];
+    for (int i = N - 2; i >= 0; --i) {
+        valley_R[i] = max(valley_R[i + 1], height[i]);
+    }
+
+    int sum = 0;
+    for (int i = 0; i < N; ++i) {
+        int min_H = min(valley_L[i], valley_R[i]);
+        int tw = min_H - height[i];
+        if (tw > 0) sum += tw;
+    }
+
+    return sum;
+}
 
 
 int main()
