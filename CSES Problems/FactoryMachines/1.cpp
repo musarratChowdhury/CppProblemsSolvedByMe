94759f8ae309a096f16016ff8247a49d0cef4ac4
musarratChowdhury <muhitlopez143@gmail.com>
2025-06-21 21:39:01 +0600

    binary search solution added

--- CSES Problems/FactoryMachines/1.cpp
+++ CSES Problems/FactoryMachines/1.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#define ll long long;
 
 using namespace std;
 
@@ -24,40 +25,61 @@ int main()
 		machine_times.push_back(x);
 		pairs[x] = 0;
 	};
-
+	//
 	sort(machine_times.begin(), machine_times.end());
 
-	while(t>0){
+	// while(t>0){
+	//
+	// 	for (int i = 0; i < machine_times.size(); ++i)
+	// 	{
+	// 		if(i == machine_times.size()-1) {
+	// 			pairs[machine_times[i]] += machine_times[i];
+	// 			break;
+	// 		}
+	// 		/* code */
+	// 		int t1 = pairs[machine_times[i]] + machine_times[i];
+	// 		int t2 = pairs[machine_times[i+1]] + machine_times[i+1];
+	//
+	// 		if(t1 <= t2){
+	// 			pairs[machine_times[i]] += machine_times[i];
+	// 			break;
+	// 		}
+	// 	}
+	//
+	//
+	// 	t--;
+	// };
 
-		for (int i = 0; i < machine_times.size(); ++i)
-		{
-			if(i == machine_times.size()-1) {
-				pairs[machine_times[i]] += machine_times[i];
-				break;
-			}
-			/* code */
-			int t1 = pairs[machine_times[i]] + machine_times[i];
-			int t2 = pairs[machine_times[i+1]] + machine_times[i+1];
+	// int max_time = 0;
+	// for (auto& [machine, time] : pairs) {
+	// 	if(time>max_time) {
+	// 		max_time = time;
+	// 	}
+ //        cout << machine << " => " << time << "\n";
+ //    }
+ //
+	// cout << max_time << "\n";
 
-			if(t1 <= t2){
-				pairs[machine_times[i]] += machine_times[i];
-				break;
-			}
+
+	long long int high = 1e18;
+	long long int low = 0;
+
+	while(low < high) {
+		long long mid = (low + high) / 2;
+
+		long long total_products = 0;
+
+		for (int i = 0; i < machine_times.size(); ++i) {
+			total_products += floor(mid/machine_times[i]);
+
+			if(total_products >= t) break;
 		}
-		
 
-		t--;
-	};
+		if(total_products >= t) high = mid;
+		else low = mid + 1;
+	}
 
-	int max_time = 0;
-	for (auto& [machine, time] : pairs) {
-		if(time>max_time) {
-			max_time = time;
-		}
-        cout << machine << " => " << time << "\n";
-    }
-
-	cout << max_time << "\n";
+	cout << low << "\n";
 
 	//total time will be max value of the pairs for each machine
 
