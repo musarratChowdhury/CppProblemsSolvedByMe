5eb637fedfd0c99a28805cc78a8042ee852d0f93
musarratChowdhury <muhitlopez143@gmail.com>
2025-07-02 09:30:10 +0600

    unsolved Ferris_Wheel/1.cpp

---
+++ CSES Problems/Ferris_Wheel/1.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	//number of children, n
+	//maximum allowed weight, x
+	int n, x;
+	cin >> n >> x;
+
+	vector<int> weights;
+	while(n>0){
+		int w;
+		cin >> w;
+		weights.push_back(w);
+		n--;
+	}
+
+	sort(weights.begin(), weights.end());
+
+	long long int total_weight = 0;
+	int gondola = 0;
+
+	for(auto& w: weights){
+		cout << w << "\n";
+		total_weight += w;
+		if(total_weight <= x)
+		{
+			//do nothing
+		}
+		else{
+			gondola++;
+			total_weight = 0;
+			total_weight += w;
+		}
+	}
+
+	cout << gondola << "\n";
+}
