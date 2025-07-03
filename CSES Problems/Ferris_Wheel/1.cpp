ff04266971987765f4dc41c64743ed8853946142
musarratChowdhury <muhitlopez143@gmail.com>
2025-07-03 23:07:38 +0600

    attempt to solve Ferris_Wheel/1.cpp

--- CSES Problems/Ferris_Wheel/1.cpp
+++ CSES Problems/Ferris_Wheel/1.cpp
@@ -22,17 +22,32 @@ int main()
 	long long int total_weight = 0;
 	int gondola = 0;
 
-	for(auto& w: weights){
-		cout << w << "\n";
-		total_weight += w;
-		if(total_weight <= x)
-		{
-			//do nothing
+	// for(auto& w: weights){
+	// 	cout << w << "\n";
+	// 	total_weight += w;
+	// 	if(total_weight <= x)
+	// 	{
+	// 		//do nothing
+	// 	}
+	// 	else{
+	// 		gondola++;
+	// 		total_weight = 0;
+	// 		total_weight += w;
+	// 	}
+	// }
+
+	for (int i = 0; i < weights.size(); ++i)
+	{
+		if(i == weights.size() - 1){
+			gondola++;
+			break;
 		}
-		else{
+		total_weight += weights[i];
+		if(total_weight + weights[i+1]  > x){
 			gondola++;
 			total_weight = 0;
-			total_weight += w;
+		}else{
+			//
 		}
 	}
 
