47665ec97fd3aa2c2b7bd5dd6271dd5a74610564
Muasarrat Chowdhury <67744581+musarratChowdhury@users.noreply.github.com>
2026-03-08 12:25:29 +0600

    attempt

---
+++ codeforces/Problems/1472B.cpp
@@ -0,0 +1,45 @@
+#include <bits//stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        int total_w = 0;
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+            total_w += arr[i];
+        }
+
+        map<int, int> onesfreq;
+        map<int, int> twosfreq;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 1) {
+                onesfreq[arr[i]]++;
+            }
+            if (arr[i] == 2) {
+                twosfreq[arr[i]]++;
+            }
+        }
+
+        if (total_w %2 != 0) {
+            cout << "NO" << endl;
+        }else {
+            if (twosfreq.size()>0 && onesfreq.size() == twosfreq.size()) {
+                cout << "YES" << endl;
+            } else if (onesfreq[1]%2==0 && twosfreq.size()==0) {
+                cout << "YES" << endl;
+            }
+            else {
+                cout << "NO" << endl;
+            }
+        }
+
+    }
+}
